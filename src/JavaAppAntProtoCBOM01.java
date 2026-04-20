9d49e2ff5fc7cc12dcb8d851070452b11bc67181
bruno <bruno@pc-prof>
2026-04-20 21:18:02 +0200

    simplification

--- src/JavaAppAntProtoCBOM01.java
+++ src/JavaAppAntProtoCBOM01.java
@@ -1,20 +1,7 @@
-import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.spec.NamedParameterSpec;
-
 public class JavaAppAntProtoCBOM01 {
-
     public static void main(String[] args) throws Exception {
-        String algo1="RSA";
-        String algo2="ML-KEM";
-        KeyPairGenerator kpg1 = KeyPairGenerator.getInstance(algo1);
-        System.out.println(kpg1.getAlgorithm()); // RSA
+        KeyPairGenerator kpg1 = KeyPairGenerator.getInstance("RSA");
         KeyPairGenerator kpg2 = KeyPairGenerator.getInstance("ML-KEM");
-        System.out.println(kpg2.getAlgorithm()); // ML-KEM
-        kpg2.initialize(new NamedParameterSpec("ML-KEM-768"));
-//        kpg2.initialize(new NamedParameterSpec("ML-KEM-1024"));
-        KeyPair kp2 = kpg2.generateKeyPair();
-        System.out.println("Public key size: " + kp2.getPublic().getEncoded().length + " bytes");
-        System.out.println("Private key size: " + kp2.getPrivate().getEncoded().length + " bytes");
     }
 }
