af3384863fb6520c3d214e8185fd2a5787a4b0ab
bruno <bruno@pc-prof>
2026-04-20 21:57:50 +0200

    sym

--- src/JavaAppAntProtoCBOM01.java
+++ src/JavaAppAntProtoCBOM01.java
@@ -1,15 +1,19 @@
 import java.security.*;
+import javax.crypto.*;
 public class JavaAppAntProtoCBOM01 {
     public static void main(String[] args) throws Exception {
-        KeyPairGenerator kpg;
-        kpg = KeyPairGenerator.getInstance("RSA");
-        kpg = KeyPairGenerator.getInstance("DH");
-        kpg = KeyPairGenerator.getInstance("ML-DSA");
-        kpg = KeyPairGenerator.getInstance("ML-KEM");
         MessageDigest md;
         md = MessageDigest.getInstance("MD5");
         md = MessageDigest.getInstance("SHA1");
         md = MessageDigest.getInstance("SHA256");
         md = MessageDigest.getInstance("SHA3-512");
+        KeyGenerator kg ;
+        kg = KeyGenerator.getInstance("DES");
+        kg = KeyGenerator.getInstance("AES");
+        KeyPairGenerator kpg;
+        kpg = KeyPairGenerator.getInstance("RSA");
+        kpg = KeyPairGenerator.getInstance("DH");
+        kpg = KeyPairGenerator.getInstance("ML-DSA");
+        kpg = KeyPairGenerator.getInstance("ML-KEM");
     }
 }
