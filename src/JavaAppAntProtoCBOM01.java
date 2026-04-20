f24a690262d1d3db6775e86d2d1c11f63146d6c5
bruno <bruno@pc-prof>
2026-04-20 18:37:00 +0200

    //

--- src/JavaAppAntProtoCBOM01.java
+++ src/JavaAppAntProtoCBOM01.java
@@ -3,6 +3,8 @@ import java.security.KeyPairGenerator;
 public class JavaAppAntProtoCBOM01 {
 
     public static void main(String[] args) throws Exception {
+        //
+        //
         String algo="RSA";
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algo);
         System.out.println(keyPairGenerator.getAlgorithm()); //RSA
